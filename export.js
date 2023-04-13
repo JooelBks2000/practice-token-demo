7921f07d4c4038672fb7eeda6f1d5479953855c0
baikaishuai <baikaishuai@tencent.com>
2023-04-13 14:18:35 +0800

    feat: add figam assets exporter

---
+++ export.js
@@ -0,0 +1,18 @@
+const figmaApiExporter = require('figma-api-exporter').default;
+
+const exporter = figmaApiExporter('figd_pH_mHW0_FNQml0PB57kCr4QUisS-HBSMQ-kSci97');
+
+//导出页面所有的svg图像
+exporter
+  .getSvgs({
+    fileId: 'TVsq0X9lXtnjsqr4LXEGNZ',
+    canvas:'Button Component'
+  })
+  .then(svgsData =>
+    exporter.downloadSvgs({
+      saveDirectory: './svgsFiles',
+      svgsData: svgsData.svgs,
+      lastModified: svgsData.lastModified,
+      clearDirectory:true
+    })
+  );
