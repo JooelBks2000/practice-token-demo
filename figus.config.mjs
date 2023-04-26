955b0139dac016399f50490d75521784c97df8a5
baikaishuai <baikaishuai@tencent.com>
2023-04-26 15:58:06 +0800

    feat: 更新figus尝试

---
+++ figus.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "@figus/cli";
+export default defineConfig({
+  output: "components",
+  path: "svgs",
+  framework: "vue",
+  figma: {
+    pageName: "Button Component",
+    fileKey: "TVsq0X9lXtnjsqr4LXEGNZ",
+    token:"figd_pH_mHW0_FNQml0PB57kCr4QUisS-HBSMQ-kSci97"
+  },
+});
